bd03b1ccca355e3e23b57643bcac3e30e90d39e3
Samu Németh <nemeth.samu.0202@gmail.com>
2025-10-09 11:56:15 +0200

    Hacked image loading.

--- src/Piece.java
+++ src/Piece.java
@@ -1,8 +1,14 @@
 import java.awt.Graphics2D;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 
 public abstract class Piece {
+    
+    protected Image image;
         
     public enum PieceColor {
         BLACK,
@@ -23,11 +29,20 @@ public abstract class Piece {
      * Show the piece image.
      */
     public void show(Graphics2D graphics, int x, int y) {
-        ImageIcon image = new ImageIcon(getAssetPath());
-        graphics.drawImage(image.getImage(), x*100, y*100, null);
+        try {
+            image = ImageIO.read(new File(getAssetPath()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if (image != null) {
+            graphics.drawImage(image, x * 100, y * 100, null);
+        } else {
+            // System.out.println("No image.");
+        }
     }
     
     public Piece(PieceColor color) {
         this.color = color;
+
     }
 }
