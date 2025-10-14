b534184fc9dee43373b69596540d157c47a437f5
Samu Németh <nemeth.samu.0202@gmail.com>
2025-10-14 18:07:58 +0200

    Added knight movement.

--- src/Knight.java
+++ src/Knight.java
@@ -26,7 +26,38 @@ public class Knight extends Piece {
     }
 
     public ArrayList<Cell> getPossibleMoves(Grid grid, Cell parentCell) {
-        return new ArrayList<Cell>();
+
+        ArrayList<Cell> possibleMoves = new ArrayList<Cell>();
+
+        int parentXpos = parentCell.getX();
+        int parentYpos = parentCell.getY();
+        
+        int[][] possibleOffsets = new int[][]{
+            {1, 2},
+            {2, 1},
+            {1, -2},
+            {2, -1},
+            {-1, 2},
+            {-2, 1},
+            {-1, -2},
+            {-2, -1},
+        };
+        
+        // Loop through all possible positions.
+        for (int[] offsets : possibleOffsets) {
+            int newXpos = parentXpos + offsets[0];
+            int newYpos = parentYpos + offsets[1];
+            
+            if (newXpos >= 0 && newXpos < Grid.SIZE && newYpos >= 0 && newYpos < Grid.SIZE) {
+                Cell toCell = grid.getCell(newXpos, newYpos);
+                if (!toCell.hasPiece() || toCell.getPiece().getColor() != color) {
+                    possibleMoves.add(toCell);
+                }
+            }
+
+        }
+        
+        return possibleMoves;
     }
 
     // public Cell[] getPossibleMoves() {
