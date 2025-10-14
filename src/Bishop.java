378e2b0d32c41f012beba6528fb5653629777087
Arnav Gupta <arnavg942@gmail.com>
2025-10-14 17:12:41 +0200

    Bishop and Queen moves imiplemented

--- src/Bishop.java
+++ src/Bishop.java
@@ -26,7 +26,64 @@ public class Bishop extends Piece {
     }
 
     public ArrayList<Cell> getPossibleMoves(Grid grid, Cell parentCell) {
-        return new ArrayList<Cell>();
+        ArrayList<Cell> possibleMoves = new ArrayList<Cell>();
+
+        int parentXpos = parentCell.getX();
+        int parentYpos = parentCell.getY();
+
+        // Down-Right Direction
+        for (int i = parentXpos + 1, j = parentYpos + 1; i < Grid.SIZE && j < Grid.SIZE; i++, j++) {
+            Cell checkCell = grid.getCell(i, j);
+            if (!checkCell.hasPiece()) {
+                possibleMoves.add(checkCell);
+            } else if (checkCell.getPiece().getColor() != color) {
+                possibleMoves.add(checkCell);
+                break;
+            } else {
+                break;
+            }
+        }
+
+        // Down-Left Direction 
+        for (int i = parentXpos - 1, j = parentYpos + 1; i >= 0 && j < Grid.SIZE; i--, j++) {
+            Cell checkCell = grid.getCell(i, j);
+            if (!checkCell.hasPiece()) {
+                possibleMoves.add(checkCell);
+            } else if (checkCell.getPiece().getColor() != color) {
+                possibleMoves.add(checkCell);
+                break;
+            } else {
+                break;
+            }
+        }
+
+        // Up-Right Direction
+        for (int i = parentXpos + 1, j = parentYpos - 1; i < Grid.SIZE && j >= 0; i++, j--) {
+            Cell checkCell = grid.getCell(i, j);
+            if (!checkCell.hasPiece()) {
+                possibleMoves.add(checkCell);
+            } else if (checkCell.getPiece().getColor() != color) {
+                possibleMoves.add(checkCell);
+                break;
+            } else {
+                break;
+            }
+        }
+
+        // Up-Left Direction
+        for (int i = parentXpos - 1, j = parentYpos - 1; i >= 0 && j >= 0; i--, j--) {
+            Cell checkCell = grid.getCell(i, j);
+            if (!checkCell.hasPiece()) {
+                possibleMoves.add(checkCell);
+            } else if (checkCell.getPiece().getColor() != color) {
+                possibleMoves.add(checkCell);
+                break;
+            } else {
+                break;
+            }
+        }
+
+        return possibleMoves;
     }
 
 
